4e5371c1d3d1603a927158d5220e95bf5c50b699
Leandro Mendoza <leandro@decentraland.org>
2022-07-11 12:36:55 -0300

    clean

--- src/systems/spawner.ts
+++ src/systems/spawner.ts
@@ -4,9 +4,9 @@ import { createCube } from '../cube'
 const { OnPointerDownResult } = engine.baseComponents
 
 export function spawnerSystem() {
-  const clickedBoshapes = engine.groupOf(SpawnerComponent, OnPointerDownResult)
+  const clickedBoxShapes = engine.groupOf(SpawnerComponent, OnPointerDownResult)
 
-  for (const [entity, spawner, pointerDownResult] of clickedBoshapes) {
+  for (const [entity, spawner, pointerDownResult] of clickedBoxShapes) {
     dcl.log(pointerDownResult.timestamp)
     if (spawner.lastPointerDownTs !== pointerDownResult.timestamp) {
       SpawnerComponent.mutable(entity).lastPointerDownTs = pointerDownResult.timestamp
@@ -14,19 +14,3 @@ export function spawnerSystem() {
     }
   }
 }
-
-// const lastPointerDownTsMap: Map<number, number> = new Map()
-
-// export function spawnerSystem() {
-//   const clickedBoshapes = engine.groupOf(OnPointerDownResult)
-
-//   for (const [entity, pointerDownResult] of clickedBoshapes) {
-//     dcl.log(pointerDownResult.timestamp)
-
-//     const lastPointerDownTs = lastPointerDownTsMap.get(entity) || 0
-//     if (lastPointerDownTs !== pointerDownResult.timestamp) {
-//       createCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1, false)
-//       lastPointerDownTsMap.set(entity, pointerDownResult.timestamp)
-//     }
-//   }
-// }
