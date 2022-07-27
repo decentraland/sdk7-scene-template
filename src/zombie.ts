4efcca6cdaf9510ca580319fff5da889ab6a9591
William Caine <william.caineiii@gmail.com>
2022-07-27 12:09:11 -0400

    attempt at sdk 6.x like syntax

--- src/zombie.ts
+++ src/zombie.ts
@@ -1,3 +1,4 @@
+import { EntityWrapper } from './backporting/EntityWrapper'
 import { MoveTransformComponent, MoveTransportDataType } from './components/moveTransport'
 
 const { Transform, GLTFShape } = engine.baseComponents
@@ -5,12 +6,21 @@ const { Transform, GLTFShape } = engine.baseComponents
 export function createZombie(): Entity {
   const zombie:Entity = engine.addEntity()
 
+  const zombieE = new EntityWrapper(zombie)
   
-  Transform.create(zombie, {
+  zombieE.addComponent(Transform,{
     position: { x: 12, y: 1, z: 3 },
     scale: { x: 1, y: 1, z: 1 },
     rotation: { x: 0, y: 0, z: 0, w: 1 }
   })
+
+  //zombieE.getComponent(Transform).position.x = 1
+  
+  /*Transform.create(zombie, {
+    position: { x: 12, y: 1, z: 3 },
+    scale: { x: 1, y: 1, z: 1 },
+    rotation: { x: 0, y: 0, z: 0, w: 1 }
+  })*/
 
   const zombeShape:PBGLTFShape = GLTFShape.create(zombie, {
     withCollisions: true,
@@ -31,6 +41,7 @@ export function createZombie(): Entity {
   })
   
 
+  //const moveTransportComponent2 = MoveTransformComponent.getFrom(zombie)
   //I can directly assign it here
   moveTransportComponent.speed = 2 
 
