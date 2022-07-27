1b7f2913771c0ba7071339409190cca244ff771a
William Caine <william.caineiii@gmail.com>
2022-07-27 11:28:17 -0400

    define the types for easier variable typing

--- src/components/moveTransport.ts
+++ src/components/moveTransport.ts
@@ -1,10 +1,28 @@
 // TODO: use higher number so we dont have conflicts (i.e. > 2000)
 const COMPONENT_ID = 2046
+
+export type Vector3Type = {
+  x:number,
+  y:number,
+  z:number
+}
+
 const Vector3EcsType = MapType({
   x: Float32,
   y: Float32,
   z: Float32
 })
+
+export type MoveTransportDataType = {
+  hasFinished: boolean,
+  duration: number,
+  start: Vector3Type,
+  end: Vector3Type,
+  normalizedTime: number,
+  lerpTime: number,
+  speed: number,
+  interpolationType: number 
+}
 
 const MoveTransportData = MapType({
   hasFinished: EcsBoolean,
--- src/zombie.ts
+++ src/zombie.ts
@@ -1,24 +1,25 @@
-import { MoveTransformComponent } from './components/moveTransport'
+import { MoveTransformComponent, MoveTransportDataType } from './components/moveTransport'
 
 const { Transform, GLTFShape } = engine.baseComponents
 
 export function createZombie(): Entity {
-  const zombie = engine.addEntity()
+  const zombie:Entity = engine.addEntity()
 
+  
   Transform.create(zombie, {
     position: { x: 12, y: 1, z: 3 },
     scale: { x: 1, y: 1, z: 1 },
     rotation: { x: 0, y: 0, z: 0, w: 1 }
   })
 
-  GLTFShape.create(zombie, {
+  const zombeShape:PBGLTFShape = GLTFShape.create(zombie, {
     withCollisions: true,
     isPointerBlocker: true,
     visible: true,
     src: 'models/zombie.glb'
   })
 
-  MoveTransformComponent.create(zombie, {
+  const moveTransportComponent:MoveTransportDataType = MoveTransformComponent.create(zombie, {
     start: { x: 12, y: 1, z: 3 },
     end: { x: 12, y: 1, z: 13 },
     duration: 10,
