b094c8190e6562f1f239389a6923ac1e8abda123
Suduck <adrian@suduck.com>
2022-09-02 13:21:12 +0200

    feat: advance test

--- src/game.ts
+++ src/game.ts
@@ -6,13 +6,28 @@ function createCube(x: number, y: number, z: number, spawner = false): Entity {
     scale: { x: 1, y: 1, z: 1 },
     rotation: { x: 0, y: 0, z: 0, w: 1 }
   })
-
-  BoxShape.create(entity)
-
+  
+  GLTFShape.create(entity, {
+    withCollisions: true,
+    isPointerBlocker: true,
+    visible: true,
+    src: 'models/zombie.glb'
+  })
+  
   if (spawner) {
-    OnPointerDown.create(entity, {
-      button: ActionButton.PRIMARY,
-      hoverText: 'Press E to spawn'
+    PointerEvents.create(entity, {
+      pointerEvents: [{eventType: PointerEventType.DOWN,
+        eventInfo: {
+          button: ActionButton.PRIMARY,
+          hoverText: 'Press E to spawn'
+        }
+      },
+        {eventType: PointerEventType.UP,
+          eventInfo: {
+            button: ActionButton.PRIMARY,
+            hoverText: 'Press E to spawn'
+          }
+        }]
     })
   }
 
@@ -27,11 +42,13 @@ function circularSystem(dt: number) {
 }
 
 function spawnerSystem() {
-  const clickedCubes = engine.getEntitiesWith(BoxShape, OnPointerDownResult)
-  for (const [] of clickedCubes) {
-    createCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+  const clickedCubes = engine.getEntitiesWith(BoxShape)
+  for (const [entity] of clickedCubes) {
+    if(wasEntityClicked(entity,ActionButton.PRIMARY))
+      createCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
   }
 }
+
 
 createCube(8, 1, 8, true)
 engine.addSystem(circularSystem)
