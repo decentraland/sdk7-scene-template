5e2991ab72308e0423a9a56d416578415efa829e
Leandro Mendoza <leandro@decentraland.org>
2023-02-06 19:43:38 -0300

    update Material and package

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ engine.addSystem(bounceScalingSystem)
 executeTask(async function () {
   // Create my main cube and color it.
   const cube = createCube(8, 1, 8)
-  Material.setPbrMaterial(cube, { albedoColor: Color4.fromHexString('#FFD96C') })
+  Material.setPbrMaterial(cube, { albedoColor: Color4.create(1.0, 0.85, 0.42) })
 })
 
 setupUi()
