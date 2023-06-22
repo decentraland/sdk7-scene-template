fcf573584555e7dba42788bfaf86de475639dc53
nearnshaw <earnshaw.nico@gmail.com>
2023-06-22 15:44:08 -0300

    use cube from inspector

--- src/systems.ts
+++ src/systems.ts
@@ -1,15 +1,16 @@
 import {
-  engine,
-  MeshRenderer,
-  Transform,
-  PointerEvents,
-  InputAction,
-  PointerEventType,
-  Schemas,
-  inputSystem
+	engine,
+	MeshRenderer,
+	Transform,
+	PointerEvents,
+	InputAction,
+	PointerEventType,
+	Schemas,
+	inputSystem
 } from '@dcl/sdk/ecs'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { createCube } from './factory'
+import { Spinner } from './components'
 
 /**
  * BounceScaling is the flag-component with the time elapsed since creation.
@@ -20,47 +21,35 @@ export const BounceScaling = engine.defineComponent('BounceScaling', { t: Schema
  * All cubes rotating behavior
  */
 export function circularSystem(dt: number) {
-  const entitiesWithMeshRenderer = engine.getEntitiesWith(MeshRenderer, Transform)
-  for (const [entity, _meshRenderer, _transform] of entitiesWithMeshRenderer) {
-    const mutableTransform = Transform.getMutable(entity)
+	const entitiesWithSpinner = engine.getEntitiesWith(Spinner, Transform)
+	for (const [entity, _spinner, _transform] of entitiesWithSpinner) {
+		const mutableTransform = Transform.getMutable(entity)
+		const spinnerData = Spinner.get(entity)
 
-    mutableTransform.rotation = Quaternion.multiply(
-      mutableTransform.rotation,
-      Quaternion.fromAngleAxis(dt * 10, Vector3.Up())
-    )
-  }
+		mutableTransform.rotation = Quaternion.multiply(
+			mutableTransform.rotation,
+			Quaternion.fromAngleAxis(dt * spinnerData.speed, Vector3.Up())
+		)
+	}
 }
 
-/**
- * The spawner system is listening for entities with hover feedback, when a input is emitted
- * just spawn a new cube randomly and animate the spawner with a bounce.
- */
-export function spawnerSystem() {
-  const clickedCubes = engine.getEntitiesWith(PointerEvents)
-  for (const [entity] of clickedCubes) {
-    if (inputSystem.isTriggered(InputAction.IA_PRIMARY, PointerEventType.PET_DOWN, entity)) {
-      createCube(1 + Math.random() * 8, Math.random() * 8, 1 + Math.random() * 8, false)
-      BounceScaling.createOrReplace(entity)
-    }
-  }
-}
 
 /**
  * Add this system and every entity with BounceScaling will bounce for 5 seconds
  * @param dt - detal time in seconds
  */
 export function bounceScalingSystem(dt: number) {
-  const clickedCubes = engine.getEntitiesWith(BounceScaling, Transform)
-  for (const [entity] of clickedCubes) {
-    const m = BounceScaling.getMutable(entity)
-    m.t += dt
+	const clickedCubes = engine.getEntitiesWith(BounceScaling, Transform)
+	for (const [entity] of clickedCubes) {
+		const m = BounceScaling.getMutable(entity)
+		m.t += dt
 
-    if (m.t > 5) {
-      Transform.getMutable(entity).scale = Vector3.One()
-      BounceScaling.deleteFrom(entity)
-    } else {
-      const factor = 0.9 + 0.2 * Math.exp(-1.5 * m.t) * Math.sin(10 * m.t)
-      Transform.getMutable(entity).scale = Vector3.scale(Vector3.One(), factor)
-    }
-  }
+		if (m.t > 5) {
+			Transform.getMutable(entity).scale = Vector3.One()
+			BounceScaling.deleteFrom(entity)
+		} else {
+			const factor = 0.9 + 0.2 * Math.exp(-1.5 * m.t) * Math.sin(10 * m.t)
+			Transform.getMutable(entity).scale = Vector3.scale(Vector3.One(), factor)
+		}
+	}
 }
