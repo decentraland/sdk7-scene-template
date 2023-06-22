5095ad39f0884bea72d215b72eede4a41a8d803e
nearnshaw <earnshaw.nico@gmail.com>
2023-06-22 16:32:34 -0300

    set tabs to 2 spaces

--- src/systems.ts
+++ src/systems.ts
@@ -1,12 +1,12 @@
 import {
-	engine,
-	MeshRenderer,
-	Transform,
-	PointerEvents,
-	InputAction,
-	PointerEventType,
-	Schemas,
-	inputSystem
+  engine,
+  MeshRenderer,
+  Transform,
+  PointerEvents,
+  InputAction,
+  PointerEventType,
+  Schemas,
+  inputSystem
 } from '@dcl/sdk/ecs'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { createCube } from './factory'
@@ -21,16 +21,16 @@ export const BounceScaling = engine.defineComponent('BounceScaling', { t: Schema
  * All cubes rotating behavior
  */
 export function circularSystem(dt: number) {
-	const entitiesWithSpinner = engine.getEntitiesWith(Spinner, Transform)
-	for (const [entity, _spinner, _transform] of entitiesWithSpinner) {
-		const mutableTransform = Transform.getMutable(entity)
-		const spinnerData = Spinner.get(entity)
+  const entitiesWithSpinner = engine.getEntitiesWith(Spinner, Transform)
+  for (const [entity, _spinner, _transform] of entitiesWithSpinner) {
+    const mutableTransform = Transform.getMutable(entity)
+    const spinnerData = Spinner.get(entity)
 
-		mutableTransform.rotation = Quaternion.multiply(
-			mutableTransform.rotation,
-			Quaternion.fromAngleAxis(dt * spinnerData.speed, Vector3.Up())
-		)
-	}
+    mutableTransform.rotation = Quaternion.multiply(
+      mutableTransform.rotation,
+      Quaternion.fromAngleAxis(dt * spinnerData.speed, Vector3.Up())
+    )
+  }
 }
 
 
@@ -39,17 +39,17 @@ export function circularSystem(dt: number) {
  * @param dt - detal time in seconds
  */
 export function bounceScalingSystem(dt: number) {
-	const clickedCubes = engine.getEntitiesWith(BounceScaling, Transform)
-	for (const [entity] of clickedCubes) {
-		const m = BounceScaling.getMutable(entity)
-		m.t += dt
+  const clickedCubes = engine.getEntitiesWith(BounceScaling, Transform)
+  for (const [entity] of clickedCubes) {
+    const m = BounceScaling.getMutable(entity)
+    m.t += dt
 
-		if (m.t > 5) {
-			Transform.getMutable(entity).scale = Vector3.One()
-			BounceScaling.deleteFrom(entity)
-		} else {
-			const factor = 0.9 + 0.2 * Math.exp(-1.5 * m.t) * Math.sin(10 * m.t)
-			Transform.getMutable(entity).scale = Vector3.scale(Vector3.One(), factor)
-		}
-	}
+    if (m.t > 5) {
+      Transform.getMutable(entity).scale = Vector3.One()
+      BounceScaling.deleteFrom(entity)
+    } else {
+      const factor = 0.9 + 0.2 * Math.exp(-1.5 * m.t) * Math.sin(10 * m.t)
+      Transform.getMutable(entity).scale = Vector3.scale(Vector3.One(), factor)
+    }
+  }
 }
