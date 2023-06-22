ee6cc063cf6e17d516e11e6a343a892d5b81a156
Gon Pombo <gonzalo@decentraland.org>
2023-06-22 16:52:05 -0300

    improve cubes created via ui (#21)

--- src/factory.ts
+++ src/factory.ts
@@ -6,25 +6,33 @@ import {
   MeshCollider,
   PointerEvents,
   PointerEventType,
-  InputAction
+  InputAction,
+  Material
 } from '@dcl/sdk/ecs'
-import { Cube } from './components'
+import { Cube, Spinner } from './components'
+import { Color4 } from '@dcl/sdk/math'
+import { getRandomHexColor } from './utils'
 
 // Cube factory
 export function createCube(x: number, y: number, z: number, spawner = true): Entity {
-  const meshEntity = engine.addEntity()
+  const entity = engine.addEntity()
 
   // Used to track the cubes
-  Cube.create(meshEntity)
+  Cube.create(entity)
 
-  Transform.create(meshEntity, { position: { x, y, z } })
+  Transform.create(entity, { position: { x, y, z } })
+
   // set how the cube looks and collides
-  MeshRenderer.setBox(meshEntity)
-  MeshCollider.setBox(meshEntity)
+  MeshRenderer.setBox(entity)
+  MeshCollider.setBox(entity)
+  Material.setPbrMaterial(entity, { albedoColor: Color4.fromHexString(getRandomHexColor()) })
+
+  // Make the cube spin, with the circularSystem
+  Spinner.create(entity, { speed: 10 * Math.random() })
 
   // if it is a spawner, then we set the pointer hover feedback
   if (spawner) {
-    PointerEvents.create(meshEntity, {
+    PointerEvents.create(entity, {
       pointerEvents: [
         {
           eventType: PointerEventType.PET_DOWN,
@@ -39,5 +47,5 @@ export function createCube(x: number, y: number, z: number, spawner = true): Ent
     })
   }
 
-  return meshEntity
+  return entity
 }
