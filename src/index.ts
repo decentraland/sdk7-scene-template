4e346ad9cfab93ea140ad2beb1081eb149112dfc
nearnshaw <earnshaw.nico@gmail.com>
2023-06-23 12:35:21 -0300

    fix indents

--- src/index.ts
+++ src/index.ts
@@ -13,29 +13,29 @@ engine.addSystem(circularSystem)
 engine.addSystem(bounceScalingSystem)
 
 export function main() {
-	// draw UI
-	setupUi()
+  // draw UI
+  setupUi()
 
-	// fetch cube from Inspector
-	const cube = engine.getEntityOrNullByName("Magic Cube")
-	if (cube) {
-		// Give the cube a color
-		Material.setPbrMaterial(cube, { albedoColor: Color4.Blue() })
+  // fetch cube from Inspector
+  const cube = engine.getEntityOrNullByName("Magic Cube")
+  if (cube) {
+    // Give the cube a color
+    Material.setPbrMaterial(cube, { albedoColor: Color4.Blue() })
 
-		// Make the cube spin, with the circularSystem
-		Spinner.create(cube, { speed: 10 })
+    // Make the cube spin, with the circularSystem
+    Spinner.create(cube, { speed: 10 })
 
-		// Give the cube a collider, to make it clickable
-		MeshCollider.setBox(cube)
+    // Give the cube a collider, to make it clickable
+    MeshCollider.setBox(cube)
 
-		// Add a click behavior to the cube, spawning new cubes in random places, and adding a bouncy effect for feedback
-		pointerEventsSystem.onPointerDown(
-			{ entity: cube, opts: { button: InputAction.IA_POINTER, hoverText: "spawn" } },
-			() => {
-				createCube(1 + Math.random() * 8, Math.random() * 8, 1 + Math.random() * 8, false)
-				BounceScaling.createOrReplace(cube)
-			}
-		)
-	}
+    // Add a click behavior to the cube, spawning new cubes in random places, and adding a bouncy effect for feedback
+    pointerEventsSystem.onPointerDown(
+      { entity: cube, opts: { button: InputAction.IA_POINTER, hoverText: "spawn" } },
+      () => {
+        createCube(1 + Math.random() * 8, Math.random() * 8, 1 + Math.random() * 8, false)
+        BounceScaling.createOrReplace(cube)
+      }
+    )
+  }
 
 }
