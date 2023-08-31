9d0e53e7d30706ef60eec07e199dc67477915895
Gonzalo DCL <gonzalo@decentraland.org>
2023-08-31 15:16:17 -0300

    add isPreview serverurl

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { InputAction, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { changeColorSystem } from './systems'
+import { getRealm } from '~system/Runtime'
 
 import { setupUi } from './ui'
 import { createCube } from './factory'
@@ -11,12 +12,10 @@ import { createNetworkTransport } from './network-transport'
 // engine.addSystem(bounceScalingSystem)
 engine.addSystem(changeColorSystem)
 
-const isPreview = true
-const serverUrl = isPreview ? 'ws://127.0.0.1:3000/ws' : 'wss://scene-state-server.decentraland.zone/ws'
-
 export async function main() {
   const entity = createCube(engine, 1, 1, 1)
-
+  const realm = await getRealm({})
+  const serverUrl = realm.realmInfo?.isPreview ? 'ws://127.0.0.1:3000/ws' : 'wss://scene-state-server.decentraland.zone/ws'
   const networkEntityFactory = await createNetworkTransport(serverUrl)
 
   // draw UI
