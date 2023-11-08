7551d611fb08c30dadf17f10fdebc1aab77f360e
Gonzalo DCL <gonzalo@decentraland.org>
2023-11-08 11:46:03 -0300

    default scene template for cli

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,26 @@
-import {
-  Animator,
-  AudioSource,
-  AvatarAttach,
-  engine,
-  GltfContainer,
-  InputAction,
-  Material,
-  MeshCollider,
-  pointerEventsSystem,
-  Transform,
-  VisibilityComponent
-} from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
+// We define the empty imports so the auto-complete feature works as expected.
+import { } from '@dcl/sdk/math'
+import { Animator, AudioSource, AvatarAttach, GltfContainer, Transform, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
 
-// You can remove this if you don't use any asset packs
-initAssetPacks(engine, pointerEventsSystem, {
-  Animator,
-  AudioSource,
-  AvatarAttach,
-  Transform,
-  VisibilityComponent,
-  GltfContainer
-})
-
-import { bounceScalingSystem, circularSystem } from './systems'
-
+import { changeColorSystem, circularSystem } from './systems'
 import { setupUi } from './ui'
-import { BounceScaling, Spinner } from './components'
-import { createCube } from './factory'
-
-// Defining behavior. See `src/systems.ts` file.
-engine.addSystem(circularSystem)
-engine.addSystem(bounceScalingSystem)
 
 export function main() {
-  // draw UI
+  // You can remove this if you don't use any asset packs
+  initAssetPacks(engine, pointerEventsSystem, {
+    Animator,
+    AudioSource,
+    AvatarAttach,
+    Transform,
+    VisibilityComponent,
+    GltfContainer
+  })
+
+  // Defining behavior. See `src/systems.ts` file.
+  engine.addSystem(circularSystem)
+  engine.addSystem(changeColorSystem)
+
+  // draw UI. Here is the logic to spawn cubes.
   setupUi()
-
-  // fetch cube from Inspector
-  const cube = engine.getEntityOrNullByName('Magic Cube')
-  if (cube) {
-    // Give the cube a color
-    Material.setPbrMaterial(cube, { albedoColor: Color4.Blue() })
-
-    // Make the cube spin, with the circularSystem
-    Spinner.create(cube, { speed: 10 })
-
-    // Give the cube a collider, to make it clickable
-    MeshCollider.setBox(cube)
-
-    // Add a click behavior to the cube, spawning new cubes in random places, and adding a bouncy effect for feedback
-    pointerEventsSystem.onPointerDown(
-      { entity: cube, opts: { button: InputAction.IA_POINTER, hoverText: 'spawn' } },
-      () => {
-        createCube(1 + Math.random() * 8, Math.random() * 8, 1 + Math.random() * 8, false)
-        BounceScaling.createOrReplace(cube)
-      }
-    )
-  }
 }
