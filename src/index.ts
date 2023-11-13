f3e86b8ee839c5e0702fded46ed86aaa87f5c71a
Gonzalo DCL <gonzalo@decentraland.org>
2023-11-13 11:11:09 -0300

    fix asset-packs order

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
 import { changeColorSystem, circularSystem } from './systems'
 import { setupUi } from './ui'
 
-export function main() {
-  // You can remove this if you don't use any asset packs
-  initAssetPacks(engine, pointerEventsSystem, {
-    Animator,
-    AudioSource,
-    AvatarAttach,
-    Transform,
-    VisibilityComponent,
-    GltfContainer
-  })
+// You can remove this if you don't use any asset packs
+initAssetPacks(engine, pointerEventsSystem, {
+  Animator,
+  AudioSource,
+  AvatarAttach,
+  Transform,
+  VisibilityComponent,
+  GltfContainer
+})
 
+export function main() {
   // Defining behavior. See `src/systems.ts` file.
   engine.addSystem(circularSystem)
   engine.addSystem(changeColorSystem)
