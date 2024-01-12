4dc598a2ddb419abdd7803199473935fb0d7a274
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-01-12 12:08:14 -0300

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // We define the empty imports so the auto-complete feature works as expected.
 import { } from '@dcl/sdk/math'
-import { Animator, AudioSource, AvatarAttach, GltfContainer, Material, Transform, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
+import { Animator, AudioSource, AvatarAttach, GltfContainer, Material, NetworkEntity, SyncComponents, Transform, Tween, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
+import { syncEntity } from '@dcl/sdk/network'
 
-import { changeColorSystem, circularSystem } from './systems'
-import { setupUi } from './ui'
+syncEntity
 
 // You can remove this if you don't use any asset packs
 initAssetPacks(engine, pointerEventsSystem, {
@@ -19,10 +19,12 @@ initAssetPacks(engine, pointerEventsSystem, {
 })
 
 export function main() {
-  // Defining behavior. See `src/systems.ts` file.
-  engine.addSystem(circularSystem)
-  engine.addSystem(changeColorSystem)
+  for (const entities of engine.getEntitiesWith(SyncComponents)) {
+    console.log('ENTITIES asd: ', entities)
+  }
 
-  // draw UI. Here is the logic to spawn cubes.
-  setupUi()
+  // syncEntity(514 as any, [Tween.componentId], 123)
+  const componentIds = SyncComponents.getMutable(516 as any).componentIds
+  componentIds.push(Tween.componentId)
+  SyncComponents.createOrReplace(516 as any, { componentIds })
 }
