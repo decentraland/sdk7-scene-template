1d7a2cdc9ab0f2f69a0269c7e519f2d24880b662
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-01-24 11:31:16 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // We define the empty imports so the auto-complete feature works as expected.
 import { } from '@dcl/sdk/math'
-import { Animator, AudioSource, AvatarAttach, GltfContainer, Material, NetworkEntity, SyncComponents, Transform, Tween, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
+import { Animator, AudioSource, AvatarAttach, Entity, GltfContainer, Material, Transform, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
-import { syncEntity } from '@dcl/sdk/network'
-
-syncEntity
 
 // You can remove this if you don't use any asset packs
 initAssetPacks(engine, pointerEventsSystem, {
@@ -19,12 +16,4 @@ initAssetPacks(engine, pointerEventsSystem, {
 })
 
 export function main() {
-  for (const entities of engine.getEntitiesWith(SyncComponents)) {
-    console.log('ENTITIES asd: ', entities)
-  }
-
-  // syncEntity(514 as any, [Tween.componentId], 123)
-  const componentIds = SyncComponents.getMutable(516 as any).componentIds
-  componentIds.push(Tween.componentId)
-  SyncComponents.createOrReplace(516 as any, { componentIds })
 }
